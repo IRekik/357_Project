fdd57e46dccb03931bd50b7ea23472894cda76b2
Farouk Ihdene <65618038+FI2000@users.noreply.github.com>
2022-04-26 19:11:46 -0400

    Adding comments
    Adding insight into the code

--- src/GUI.java
+++ src/GUI.java
@@ -16,10 +16,12 @@ import javax.swing.JRadioButton;
 import javax.swing.JTextField;
 
 public class GUI {
-
+    
+    //Making player objects to use later
     MP3FileManager am = new MP3FileManager();
     MediaPlayer player = new MediaPlayer();
-
+    
+    //Setting up variables
     static int difficulty = 0;
     static boolean learning = true;
     static String answer1 = "";
@@ -42,24 +44,23 @@ public class GUI {
         answer8 = "";
         correct = 0;
     }
-
+    //Method to create a new button
     private JButton createButton(String s) {
         JButton button = new JButton(s);
-
         return button;
     }
-
+    //Setter to change the difficulty of the learning exercise
     private void setDifficulty(int x) {
         difficulty = x;
     }
-
+    //Setter to change from learning mode to challenge mode
     private void setLearning(boolean x) {
         learning = x;
     }
 
 
     public GUI() {
-
+        //Initializing the panel and frame to use later
         JFrame frame = new JFrame();
         JPanel panel = new JPanel();
         frame.setTitle("Learning by Ear");
@@ -67,10 +68,11 @@ public class GUI {
         frame.setSize(600,350);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
-
+        
+        //Forming the main interface GUI of the selection screen
         JLabel title = new JLabel("Learning by Ear");	title.setFont(new Font("Verdana",1,30));	title.setBounds(165, 0, 400, 100);
         JLabel difficulty = new JLabel("Difficulty");	difficulty.setFont(new Font("Verdana",1,20));	difficulty.setBounds(235, 65, 400, 100);
-
+        
         JRadioButton option1 = new JRadioButton("To Learn");
         JRadioButton option2= new JRadioButton("Challenge");
         ButtonGroup radiogroup = new ButtonGroup();
@@ -81,11 +83,12 @@ public class GUI {
         option1.setFocusable(false);
         option2.setFocusable(false);
 
-
+        //Creating the buttons
         JButton button1 = createButton("Easy");	button1.setBounds(60, 200, 130, 50);	button1.setFocusable(false);
         JButton button2 = createButton("Medium");	button2.setBounds(230, 200, 130, 50);	button2.setFocusable(false);
         JButton button3 = createButton("Hard");	button3.setBounds(400, 200, 130, 50);	button3.setFocusable(false);
-
+        
+        //Giving buttons actions when clicking on them
         button1.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 frame.dispose();
@@ -110,7 +113,7 @@ public class GUI {
                 MusicalTraining();
             }
         });
-
+        //This is a radio button listener to switch between learning mode and challenge mode
         option1.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 setLearning(true);
@@ -122,7 +125,8 @@ public class GUI {
                 setLearning(false);
             }
         });
-
+        
+        //Adding everything to the panel
         panel.setLayout(null);
         panel.add(title);
         panel.add(difficulty);
@@ -134,7 +138,7 @@ public class GUI {
 
         frame.add(panel, BorderLayout.CENTER);
     }
-
+    //Custom made method to randomize 4 integers, used for choosing 4 random note sounds from a sample of 12
     public int[] get4Numbers() {
         Random rand = new Random();
         boolean finished = false;
@@ -175,7 +179,7 @@ public class GUI {
         return arr;
 
     }
-
+    //Custom made method to randomize 6 integers, used for choosing 6 random note sounds from a sample of 12
     public int[] get6Numbers() {
         Random rand = new Random();
         boolean finished = false;
@@ -234,7 +238,8 @@ public class GUI {
         return arr;
 
     }
-
+    
+    //Custom made method to randomize 8 integers, used for choosing 8 random note sounds from a sample of 12
     public int[] get8Numbers() {
         Random rand = new Random();
         boolean finished = false;
@@ -312,6 +317,7 @@ public class GUI {
 
     }
 
+       //Custom method to check if the input answer is correct with the one attributed to the button
     public boolean right(String s, JButton b) {
         if (s.equals(b.getName())) {
             correct++;
@@ -319,7 +325,9 @@ public class GUI {
         }
         return false;
     }
-
+    
+    //Method that produces an array with n integers ordered randomly
+    //It is used to randomize the button order for practicing the same notes for learning
     public int[] order(int n) {
         int[] arr = null;
         if(n==1) {
@@ -328,7 +336,7 @@ public class GUI {
             for (int i=0; i<4; i++) {
                 list.add(i);
             }
-            Collections.shuffle(list);
+            Collections.shuffle(list); // Used to randomize the order
             for (int i=0; i<list.size(); i++) {
                 arr[i]=list.get(i);
             }
@@ -359,6 +367,7 @@ public class GUI {
         return arr;
     }
 
+    //Method that returns the pixel position value on the horizontal axis based on the difficulty and position of the button
     public int Xvalue(int n, int m) {
         int x = 0;
         if(n==1) {
@@ -387,7 +396,8 @@ public class GUI {
         }
         return x;
     }
-
+    
+    //Method that returns the pixel position value on the vertical axis based on the difficulty and position of the button
     public int Yvalue(int n, int m) {
         int x = 0;
         if(n==1) {
@@ -405,14 +415,17 @@ public class GUI {
         return x;
     }
     public void MusicalTraining() {
+        //Setting up new window for the part where you play the exercise
         JFrame MusicalFrame = new JFrame();
         JPanel MusicalPanel = new JPanel();
         JButton back = new JButton();
         MusicalFrame.setTitle("Learning by Ear");
         MusicalFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-
+        
+        //Creating the 12 notes that will be used for the exercise
         ArrayList<JButton> NoteList = new ArrayList<JButton>();
         if(learning == true) {
+            //In the learning section, you can see the names to get used to the sound they make
             JButton note1 = createButton("A"); 	note1.setName("A");
             JButton note2 = createButton("A#"); note2.setName("A#");
             JButton note3 = createButton("B"); note3.setName("B");
@@ -428,6 +441,8 @@ public class GUI {
             NoteList.add(note1);NoteList.add(note2);NoteList.add(note3);NoteList.add(note4);
             NoteList.add(note5);NoteList.add(note6);NoteList.add(note7);NoteList.add(note8);
             NoteList.add(note9);NoteList.add(note10);NoteList.add(note11);NoteList.add(note12);
+            
+            //Giving the notes (buttons) the action of producing their specific sound
             for (int i = 0; i < NoteList.size(); i++) {
                 int finalI = i;
                 NoteList.get(i).addActionListener(new ActionListener() {
@@ -438,6 +453,7 @@ public class GUI {
             }
         }
         else {
+            //In the challenge section, you cannot see the names, you are supposed to guess them according to the sound they make
             JButton note1 = createButton("?"); note1.setName("A");
             JButton note2 = createButton("?"); note2.setName("A#");
             JButton note3 = createButton("?"); note3.setName("B");
@@ -453,6 +469,8 @@ public class GUI {
             NoteList.add(note1);NoteList.add(note2);NoteList.add(note3);NoteList.add(note4);
             NoteList.add(note5);NoteList.add(note6);NoteList.add(note7);NoteList.add(note8);
             NoteList.add(note9);NoteList.add(note10);NoteList.add(note11);NoteList.add(note12);
+            
+            //Giving the notes (buttons) the action of producing their specific sound
             for (int i = 0; i < NoteList.size(); i++) {
                 int finalI = i;
                 NoteList.get(i).addActionListener(new ActionListener() {
@@ -462,12 +480,15 @@ public class GUI {
                 });
             }
         }
-
+        
+        //Making text fields to allow users to write down their answers
         JTextField tf1 = new JTextField();JTextField tf2 = new JTextField(); JTextField tf3 = new JTextField();JTextField tf4 = new JTextField();
         JTextField tf5 = new JTextField(); JTextField tf6 = new JTextField(); JTextField tf7 = new JTextField(); JTextField tf8 = new JTextField();
+        //Adding the submit and practice buttons
         JButton submit = createButton("Submit");	submit.setBounds(450, 250, 130, 50);	submit.setFocusable(false); MusicalPanel.add(submit);
         JButton next = createButton("Practice");	next.setBounds(450, 250, 130, 50);	next.setFocusable(false); MusicalPanel.add(next);
         JLabel Score =new JLabel();
+        //Here you generate the 8 random numbers from 1 to 12 that will choose which notes are displayed on the buttons, then we create buttons that point to them
         int[] arr = get8Numbers();
         JButton b1 = NoteList.get(arr[0]); JButton b2 = NoteList.get(arr[1]); JButton b3 = NoteList.get(arr[2]); JButton b4 = NoteList.get(arr[3]);
         JButton b5 = NoteList.get(arr[4]); JButton b6 = NoteList.get(arr[5]); JButton b7 = NoteList.get(arr[6]); JButton b8 = NoteList.get(arr[7]);
@@ -476,15 +497,17 @@ public class GUI {
 
         submit.setVisible(false);
         next.setVisible(false);
-
+        
+        //If we are learning, we want to be able to go to the practice section, if not we can just submit the answers since we are already in challenge mode
         if(learning) {next.setVisible(true);} else {submit.setVisible(true);}
         //DIFFICULTY : EASY
         if(GUI.difficulty == 1) {
+            //Easy difficulty has 4 notes
             b1.setBounds(100, 20, 75, 50); b2.setBounds(400, 20, 75, 50);
             b3.setBounds(100, 140, 75, 50); b4.setBounds(400, 140, 75, 50);
             MusicalPanel.add(b1);MusicalPanel.add(b2);MusicalPanel.add(b3);MusicalPanel.add(b4);
             if (!learning) {
-
+                //If we are in challenge mode, we have 4 textfields
                 tf1.setBounds(100, 75, 75, 25); tf2.setBounds(400, 75, 75, 25);
                 tf3.setBounds(100, 195, 75, 25); tf4.setBounds(400, 195, 75, 25);
                 MusicalPanel.add(tf1);MusicalPanel.add(tf2);MusicalPanel.add(tf3);MusicalPanel.add(tf4);}
@@ -510,23 +533,24 @@ public class GUI {
                 MusicalPanel.add(tf1);MusicalPanel.add(tf2);MusicalPanel.add(tf3);MusicalPanel.add(tf4);
                 MusicalPanel.add(tf5);MusicalPanel.add(tf6);MusicalPanel.add(tf7);MusicalPanel.add(tf8);}
         }
-
+        
+        //The practice button is used to randomize the place of the current notes, hide their name and allow for submitting answers
         next.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 next.setVisible(false);
                 submit.setVisible(true);
                 if(GUI.difficulty == 1) {
-                    b1.setText("?");b2.setText("?");b3.setText("?");b4.setText("?");
+                    b1.setText("?");b2.setText("?");b3.setText("?");b4.setText("?"); //Make the name invisible
                     int[] arr = order(1);
                     for(int i=0;i<arr.length;i++) {
                         int x = Xvalue(1,arr[i]);
                         int y = Yvalue(1,arr[i]);
-                        if(i==0) {b1.setBounds(x, y, 75, 50); tf1.setBounds(x, y+55, 75, 25);}
+                        if(i==0) {b1.setBounds(x, y, 75, 50); tf1.setBounds(x, y+55, 75, 25);} //Here we are changing the position of the notes in a random manner
                         else if(i==1){b2.setBounds(x, y, 75, 50); tf2.setBounds(x, y+55, 75, 25); }
                         else if(i==2){b3.setBounds(x, y, 75, 50); tf3.setBounds(x, y+55, 75, 25);}
                         else if(i==3){b4.setBounds(x, y, 75, 50); tf4.setBounds(x, y+55, 75, 25);}
                     }
-                    MusicalPanel.add(tf1);MusicalPanel.add(tf2);MusicalPanel.add(tf3);MusicalPanel.add(tf4);
+                    MusicalPanel.add(tf1);MusicalPanel.add(tf2);MusicalPanel.add(tf3);MusicalPanel.add(tf4); //Adding only 4 text fields (we have 4 notes)
                 }
                 else if (GUI.difficulty == 2) {
                     b1.setText("?");b2.setText("?");b3.setText("?");b4.setText("?");b5.setText("?");b6.setText("?");
@@ -541,6 +565,7 @@ public class GUI {
                         else if(i==4){b5.setBounds(x, y, 75, 50);tf5.setBounds(x, y+55, 75, 25);}
                         else if(i==5){b6.setBounds(x, y, 75, 50);tf6.setBounds(x, y+55, 75, 25);}
                     }
+                    //Adding only 6 text fields (we have 6 notes)
                     MusicalPanel.add(tf1);MusicalPanel.add(tf2);MusicalPanel.add(tf3);MusicalPanel.add(tf4);MusicalPanel.add(tf5);MusicalPanel.add(tf6);
                 }
                 else if (GUI.difficulty == 3) {
@@ -558,6 +583,7 @@ public class GUI {
                         else if(i==6){b7.setBounds(x, y, 75, 50);tf7.setBounds(x, y+55, 75, 25);}
                         else if(i==7){b8.setBounds(x, y, 75, 50);tf8.setBounds(x, y+55, 75, 25);}
                     }
+                    //Adding 8 text fields 
                     MusicalPanel.add(tf1);MusicalPanel.add(tf2);MusicalPanel.add(tf3);MusicalPanel.add(tf4);
                     MusicalPanel.add(tf5);MusicalPanel.add(tf6);MusicalPanel.add(tf7);MusicalPanel.add(tf8);
                 }
@@ -568,9 +594,11 @@ public class GUI {
         //SUBMIT BUTTON ACTION
         submit.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
+                //Here we set the text fields to invisible and check the answers.
                 tf1.setVisible(false);tf2.setVisible(false);tf3.setVisible(false);tf4.setVisible(false);tf5.setVisible(false);tf6.setVisible(false);tf7.setVisible(false);tf8.setVisible(false);
                 answer1 = tf1.getText();
-                if(right(answer1,b1)) {	b1.setForeground(Color.GREEN);} else {b1.setForeground(Color.RED);}
+                //Here we compare the answer given with the name of the button
+                if(right(answer1,b1)) {	b1.setForeground(Color.GREEN);} else {b1.setForeground(Color.RED);} //If the answer is correct. the text becomes green, else it is red
                 b1.setText(b1.getName());
                 answer2 = tf2.getText();
                 if(right(answer2,b2)) {	b2.setForeground(Color.GREEN);} else {b2.setForeground(Color.RED);}
@@ -615,14 +643,14 @@ public class GUI {
             }
         });
 
-
-
+        //Setting up the frame
+    
         MusicalFrame.setLocationRelativeTo(null);
         MusicalFrame.setVisible(true);
         MusicalPanel.setLayout(null);
         MusicalPanel.add(back);
         MusicalFrame.add(MusicalPanel, BorderLayout.CENTER);
-
+        //Back button's action
         back.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 MusicalFrame.dispose();
